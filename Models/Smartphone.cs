a82b347ba59b29085dd3568baef1fa6329684f00
roodev <rodrigo.fessore@gmail.com>
2023-09-28 11:05:09 -0300

    Criando projeto console e definindo as classes que serão utilizadas no desafio

---
+++ Models/Smartphone.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio_Celular.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public Smartphone(string numero)
+        {
+            Numero = numero;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+        
+    }
+}
